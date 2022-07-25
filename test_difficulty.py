e4050782be8bc2633a0d050d7f60fc0c763e704f
Dave2000sang <david2000sang@hotmail.com>
2022-07-25 00:19:08 -0400

    add more easy words

--- test_difficulty.py
+++ test_difficulty.py
@@ -1,19 +1,28 @@
-from utils import get_word_difficulty
+from utils import get_word_difficulty, init_mongo
 from collections import OrderedDict
 
-wordset = OrderedDict()
-with open('hard_words.txt', 'r') as f:
-    for word in f:
-        word = word.strip()
-        wordset[word] = None
+# wordset = OrderedDict()
+# with open('easy_words.txt', 'r') as f:
+#     for word in f:
+#         word = word.strip()
+#         wordset[word] = None
 
-# remove duplicates
-with open('hard_words.txt', 'w') as f:
-    for word in wordset.keys():
-        f.write(f"{word}\n")
+# # # remove duplicates
+# # with open('easy_words.txt', 'w') as f:
+# #     for word in wordset.keys():
+# #         f.write(f"{word}\n")
 
-with open('hard_words_difficulty.txt', 'w') as f:
-    for word in wordset.keys():
-        diff = get_word_difficulty(word)
-        print(f"{word} - {diff}")
-        f.write(f"{word}: {diff}\n")
+# with open('easy_words_difficulty.txt', 'w') as f:
+#     for word in wordset.keys():
+#         diff = get_word_difficulty(word)
+#         print(f"{word} - {diff}")
+#         f.write(f"{word}: {diff}\n")
+
+
+# what the distribution like
+mongo = init_mongo()
+easy = len(list(mongo.spellingo.words_us.find({'difficulty': 'easy'})))
+medium = len(list(mongo.spellingo.words_us.find({'difficulty': 'medium'})))
+hard = len(list(mongo.spellingo.words_us.find({'difficulty': 'hard'})))
+
+print(easy, medium, hard)
